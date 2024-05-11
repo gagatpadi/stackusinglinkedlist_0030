6bb1bd40a4f4912949fb051144d00106a6b32954
gagatpadi <padiyogaradhana@gmail.com>
2024-05-11 10:54:12 +0700

    int choice

--- stackusinglinkedlist/stackusinglinkedlist.cpp
+++ stackusinglinkedlist/stackusinglinkedlist.cpp
@@ -44,4 +44,31 @@ public:
 	}
 
 
+	void peek() {
+		if (top == NULL) {
+			cout << "List is empty." << endl;
+		}
+		else {
+			Node* current = top;
+			while (current != NULL) {
+				cout << current->data << " " << endl;
+				current = current->next;
+			}
+			cout << endl;
+		}
+	}
+
+
+	bool isEmpty() {
+		return top == NULL;
+	}
+
+};
+
+int main() {
+	Stack stack;
+
+	int choice = 0;
+	int value;
+
 	
