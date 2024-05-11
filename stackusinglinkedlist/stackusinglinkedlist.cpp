e1404e913f9b4d9cb11ec5775b64cd1d75136c08
gagatpadi <padiyogaradhana@gmail.com>
2024-05-11 10:37:46 +0700

    void peek

--- stackusinglinkedlist/stackusinglinkedlist.cpp
+++ stackusinglinkedlist/stackusinglinkedlist.cpp
@@ -42,4 +42,21 @@ public:
 		top = top->next;
 
 	}
+
+
+	void peek() {
+		if (top == NULL) {
+			cout << "List is empty." << endl;
+		}
+		else {
+			Node* current = top;
+			while (current != NULL) {
+				cout << current->data << " " << endl;
+				current = current->next;
+			}
+			cout << endl;
+		}
+	}
+
+
 };
