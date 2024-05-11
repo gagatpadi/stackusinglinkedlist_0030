4eafebd3ddd8e901358d4fc934a7a605358180a4
gagatpadi <padiyogaradhana@gmail.com>
2024-05-11 10:53:48 +0700

    void pop

--- stackusinglinkedlist/stackusinglinkedlist.cpp
+++ stackusinglinkedlist/stackusinglinkedlist.cpp
@@ -8,4 +8,40 @@ public:
 	int data;
 	Node* next;
 	
+	Node() {
+		next = NULL;
+	}
+};
+
+
+class Stack {
+private:
+	Node* top;
+
+public:
+	Stack() {
+		top = NULL;
+	}
+
+
+	int push(int value) {
+		Node* newNode = new Node();
+		newNode->data = value;
+		newNode->next = top;
+		top = newNode;
+		cout << "Push Value: " << value << endl;
+		return value;
+	}
+
+
+	void pop() {
+		if (isEmpty()) {
+			cout << "Stack is empty." << endl;
+		}
+		cout << "Popped value: " << top->data << endl;
+		top = top->next;
+
+	}
+
+
 	
